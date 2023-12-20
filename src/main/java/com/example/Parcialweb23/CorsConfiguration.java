3f9711de70c0bcc7b0ebd953775445eae953bbae
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2023-12-20 10:27:10 -0500

    2

--- src/main/java/com/example/Parcialweb23/CorsConfiguration.java
+++ src/main/java/com/example/Parcialweb23/CorsConfiguration.java
@@ -1,11 +1,28 @@
 package com.example.Parcialweb23;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableWebMvc
 public class CorsConfiguration implements WebMvcConfigurer {
 
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
+                .allowCredentials(false);
+    }
 
+    // Otro código de configuración y métodos adicionales pueden ir aquí
+
+    // Ejemplo de configuración de beans para otras funcionalidades (si es necesario)
+    // @Bean
+    // public SomeBean someBean() {
+    //     return new SomeBean();
+    // }
 }
