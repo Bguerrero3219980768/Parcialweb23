1794884ec8aa7bfcbdbebc6501d1be99ef6ee3d6
brayanguerreo123 <112359242+Bguerrero3219980768@users.noreply.github.com>
2023-12-20 10:32:49 -0500

    3

--- src/main/java/com/example/Parcialweb23/controller/EntrenadorController.java
+++ src/main/java/com/example/Parcialweb23/controller/EntrenadorController.java
@@ -1,19 +1,43 @@
 package com.example.Parcialweb23.controller;
 
+import com.example.ParcialWeb23.entities.Entrenador;
+import com.example.ParcialWeb23.entities.Pokemon;
+import com.example.ParcialWeb23.repositories.EntrenadorRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.Parcialweb23.entities.Entrenador;
-import com.example.Parcialweb23.entities.Pokemon;
-import com.example.Parcialweb23.repositories.EntrenadorRepository;
-
-import javax.swing.text.html.Option;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
 @RequestMapping("/entrenador")
 public class EntrenadorController {
-   
+
+    @Autowired
+    private EntrenadorRepository entrenadorRepository;
+
+    @GetMapping("/all")
+    public List<Entrenador> getAllEntrenadores() {
+        return entrenadorRepository.findAll();
+    }
+
+    @GetMapping("/login/{email}")
+    public ResponseEntity<Entrenador> getEntrenadorByEmail(@PathVariable String email) {
+        Optional<Entrenador> entrenador = entrenadorRepository.findById(email);
+
+        return entrenador.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @GetMapping("/{uuid}/pokemon")
+    public ResponseEntity<List<Pokemon>> getPokemonByEntrenadorUuid(@PathVariable String uuid) {
+        Optional<Entrenador> entrenador = entrenadorRepository.findByUuid(uuid);
+
+        return entrenador.map(e -> ResponseEntity.ok(e.getPokemones()))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Puedes agregar otros métodos según sea necesario
+
 }
--- src/main/java/com/example/Parcialweb23/controller/PokemonController.java
+++ src/main/java/com/example/Parcialweb23/controller/PokemonController.java
@@ -1,12 +1,10 @@
 package com.example.Parcialweb23.controller;
 
+import com.example.ParcialWeb23.entities.Pokemon;
+import com.example.ParcialWeb23.repositories.PokemonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import com.example.Parcialweb23.entities.Entrenador;
-import com.example.Parcialweb23.entities.Pokemon;
-import com.example.Parcialweb23.repositories.EntrenadorRepository;
-import com.example.Parcialweb23.repositories.PokemonRepository;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,5 +13,22 @@ import java.util.Optional;
 @RequestMapping("/pokemon")
 public class PokemonController {
 
+    @Autowired
+    private PokemonRepository pokemonRepository;
+
+    @GetMapping("/all")
+    public List<Pokemon> getAllPokemon() {
+        return pokemonRepository.findAll();
+    }
+
+    @GetMapping("/{tipo}")
+    public ResponseEntity<Pokemon> getPokemonByType(@PathVariable Integer tipo) {
+        Optional<Pokemon> pokemon = pokemonRepository.findByTipoPokemon(tipo);
+
+        return pokemon.map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    // Puedes agregar otros métodos según sea necesario
 
 }
